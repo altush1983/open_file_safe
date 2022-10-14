005c07642358d19bc4103152c82afa068f57b5cc
Alexey Tushurashvili <altush@yandex.ru>
2022-10-14 14:58:26 +0300

    replace permission.REQUEST_INSTALL_PACKAGES to Timber warning

--- android/src/main/java/com/crazecoder/openfile/OpenFilePlugin.java
+++ android/src/main/java/com/crazecoder/openfile/OpenFilePlugin.java
@@ -39,6 +39,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
+import timber.log.Timber;
+
 
 /**
  * OpenFilePlugin
@@ -108,7 +110,7 @@ public class OpenFilePlugin implements MethodCallHandler
                 }
                 if (hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
                     if (TYPE_STRING_APK.equals(typeString)) {
-                        openApkFile();
+                        Timber.w("open_file: APK files not supported");
                         return;
                     }
                     startActivity();
@@ -348,28 +350,6 @@ public class OpenFilePlugin implements MethodCallHandler
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void openApkFile() {
-        if (!canInstallApk()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startInstallPermissionSettingActivity();
-            } else {
-                ActivityCompat.requestPermissions(activity,
-                        new String[]{Manifest.permission.REQUEST_INSTALL_PACKAGES}, REQUEST_CODE);
-            }
-        } else {
-            startActivity();
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private boolean canInstallApk() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            return activity.getPackageManager().canRequestPackageInstalls();
-        }
-        return hasPermission(Manifest.permission.REQUEST_INSTALL_PACKAGES);
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void startInstallPermissionSettingActivity() {
         if (activity == null) {
@@ -384,9 +364,8 @@ public class OpenFilePlugin implements MethodCallHandler
     @Override
     public boolean onRequestPermissionsResult(int requestCode, String[] strings, int[] grantResults) {
         if (requestCode != REQUEST_CODE) return false;
-        if (hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                && TYPE_STRING_APK.equals(typeString)) {
-            openApkFile();
+        if (TYPE_STRING_APK.equals(typeString)) {
+            Timber.w("open_file: APK files not supported");
             return false;
         }
         for (String string : strings) {
@@ -403,11 +382,7 @@ public class OpenFilePlugin implements MethodCallHandler
     @Override
     public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == RESULT_CODE) {
-            if (canInstallApk()) {
-                startActivity();
-            } else {
-                result(-3, "Permission denied: " + Manifest.permission.REQUEST_INSTALL_PACKAGES);
-            }
+            Timber.w("open_file: APK files not supported (onActivityResult)");
         }
         return false;
     }
